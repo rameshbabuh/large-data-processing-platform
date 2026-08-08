631af2971b829c59cc705403f6058d15b67fece0
rameshbabuh <rameshbabu9889@gmail.com>
2026-08-07 20:03:53 -0400

    Checking in unversioned files

---
+++ backend/data-processing-api/src/main/java/com/ramesh/dataprocessing/DataProcessingApiApplication.java
@@ -0,0 +1,13 @@
+package com.ramesh.dataprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataProcessingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataProcessingApiApplication.class, args);
+    }
+
+}
